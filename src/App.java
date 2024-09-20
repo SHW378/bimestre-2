141161f2d12a941bb4f745c6357be0c9fea6c65d
SHW378 <cesarbrmn@hotmail.com>
2024-09-20 15:53:35 -0600

    1.1

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        //condicionales
+        int x = 100;
+        int f = 0;
+
+        if  (x > 0); {
+            f = x * x;
         
+        }
+        System.out.println("El valor es: " + f);
+        //condicionales
     }
 }
