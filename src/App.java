2134914d0e4f02c6d280a0f441529932c8260a76
SHW378 <cesarbrmn@hotmail.com>
2024-09-23 17:38:36 -0600

    1.3

--- src/App.java
+++ src/App.java
@@ -3,11 +3,11 @@ public class App {
         //condicionales
         int x = 100;
         int f = 0;
-
-        if  (x > 0); {
+        // Si x es mayor a 0, hacer f igual a x al cuadrado
+        if  (x > 0); { //bloque de setencia condocional
             f = x * x;
         
-        }
+        } //fin de bloque de sentencia condicionañ
         System.out.println("El valor es: " + f);
         //condicionales
     }
