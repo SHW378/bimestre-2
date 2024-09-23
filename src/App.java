79f20125a116908c8ca3d7048143966d13932101
SHW378 <cesarbrmn@hotmail.com>
2024-09-23 17:54:50 -0600

    1.5

--- src/App.java
+++ src/App.java
@@ -1,13 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
         //condicionales
-        int x = 100;
-        int f = 0;
+        double x = 100;
+        double f = 0;
         // Si x es mayor a 0, hacer f igual a x al cuadrado
         //si no, si x es mayor a 0, hacer f igual a x al ciadrado
         //Si no, entonces hacer f igual al valor avsoluto de x
-        if  (x > 0); { //bloque de setencia condocional
-            f = x * x;
+        if  (x > 0) { //bloque de setencia condocional
+            f = Math.pow(x, 3);
         } else if (x > 0) {
             f = x * x;
         } else { 
