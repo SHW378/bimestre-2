ec2a63a6eff15db0d8165bb4730594d966e8d0ff
SHW378 <cesarbrmn@hotmail.com>
2024-09-23 17:57:53 -0600

    1.51

--- src/App.java
+++ src/App.java
@@ -6,7 +6,7 @@ public class App {
         // Si x es mayor a 0, hacer f igual a x al cuadrado
         //si no, si x es mayor a 0, hacer f igual a x al ciadrado
         //Si no, entonces hacer f igual al valor avsoluto de x
-        if  (x > 0) { //bloque de setencia condocional
+        if  (x > 100) { //bloque de setencia condocional
             f = Math.pow(x, 3);
         } else if (x > 0) {
             f = x * x;
