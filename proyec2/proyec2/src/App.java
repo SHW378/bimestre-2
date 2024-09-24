8ab6c52ee9ee41072daebe7c90fd8e6e8be8f22c
SHW378 <cesarbrmn@hotmail.com>
2024-09-23 23:01:47 -0600

    proy2 1.0

---
+++ proyec2/proyec2/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        double x = 0;
+        if (x > 0) {
+        System.out.println("el numero tiene signo positivo");
+    } else if (x < 0) {
+        System.out.println("El numero tiene signo negativo");
+    } else {
+        System.out.println("El numero es nulo");          
+    }
+
+    }
+}
