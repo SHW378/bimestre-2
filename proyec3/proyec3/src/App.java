498c38200acc7c577373097abbce56d595602a01
SHW378 <cesarbrmn@hotmail.com>
2024-09-30 18:52:54 -0600

    ---

--- proyec3/proyec3/src/App.java
+++ proyec3/proyec3/src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         double a  = 0;
-        double b = 0;
+        double b = 1;
         char s = '+';
 
         if (s == '+') {
