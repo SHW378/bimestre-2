0c1f5e68e68405d9ce2e232946a9bde36bca6fb7
SHW378 <cesarbrmn@hotmail.com>
2024-09-30 18:07:14 -0600

    1p2-1

---
+++ proyt2-1/p2-1/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int mm = 2;
+        if (mm == 1 || mm == 2 || mm == 3)
+        System.out.println("Corresponde al primer trimestre");
+    }
+}
